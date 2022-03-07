1b46813668974a68c4cbb67bad4076b94b100909
Andrew McNeill <andrew@andrewmc.ca>
2022-03-07 19:57:07 -0400

    finishes exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum = Store.sum('annual_revenue')
+puts "Total Revenue: #{sum}"
+count = Store.count
+puts "Average Revenue: #{sum / count}"
+
+puts Store.where('annual_revenue > 1000000').count
